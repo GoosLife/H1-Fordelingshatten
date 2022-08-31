d546b05c5e2e404500b0bdd4efafb73d2db61022
GoosLife <72657308+GoosLife@users.noreply.github.com>
2022-08-31 08:33:10 +0200

    Add project files.

---
+++ H1 - Fordelingshatten/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace H1___Fordelingshatten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortingHat sh = new SortingHat(); // Creating sorting hat object
+            Console.WriteLine(sh.ChooseHouse()); // Choose a random house and print it to console.
+            Console.ReadKey();
+        }
+    }
+}
---
+++ H1 - Fordelingshatten/SortingHat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1___Fordelingshatten
+{
+    enum House
+    {
+        Gryffindor,
+        Hufflepuff,
+        Ravenclaw,
+        Slytherin
+    }
+
+    internal class SortingHat
+    {
+        /// <summary>
+        /// What no one will tell you is that most of the time,
+        /// the sorting hat is just picking houses at random.
+        /// </summary>
+        /// <returns></returns>
+        public House ChooseHouse()
+        {
+            // Inline solution
+            // return Enum.GetValues<House>()[new Random().Next(Enum.GetValues<House>().Length)];
+
+            // More readable solution
+            Random r = new Random();
+
+            House[] houseArray = Enum.GetValues<House>(); // Store all enum values of type house in an array.
+
+            int randomIndex = r.Next(houseArray.Length); // Choose a random number between 0 and the number of houses -1.
+
+            House h = houseArray[randomIndex]; // Get the randomly chosen house from the array.
+            return h; // Return that house.
+        }
+    }
+}
